d1d5af598f2db5b5167f65f39ccb11d7944c8065
DinosaurioRawn <dulceyadira.cruzgonzalez@gmail.com>
2025-12-03 09:05:44 -0600

    Pagina de catalogo

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,3 +1,4 @@
 export * from "./HomePage";
 export * from "./StorePage";
 export * from "./AboutPage";
+export * from "./StoresPage";
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
 import { RootLayout } from "../layouts/RootLayout";
-import { AboutPage, HomePage, StorePage } from "../pages";
+import { AboutPage, HomePage, StorePage, StoresPage, } from "../pages";
 
 export const router = createBrowserRouter([
     {
         path: '/',
-        element: <RootLayout/>,
-        children:[
+        element: <RootLayout />,
+        children: [
             {
                 index: true,
                 element: <HomePage />,
@@ -16,8 +16,12 @@ export const router = createBrowserRouter([
                 element: <StorePage />,
             },
             {
+                path: 'product/:slug',
+                element: <StoresPage />,
+            },
+            {
                 path: 'nosotros',
-                element: <AboutPage/>,
+                element: <AboutPage />,
             }
         ]
     },
